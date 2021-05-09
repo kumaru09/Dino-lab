ff36468eb62e9601cfc3f8459040b0e04692425c
Ausanee26 <60252895+Ausanee26@users.noreply.github.com>
2021-05-10 00:41:33 +0700

    Booking Page

---
+++ Controllers/BookController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Dinolab.Models;
+
+namespace Dinolab.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ILogger<BookController> _logger;
+
+        public BookController(ILogger<BookController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -9,7 +9,7 @@ var regModal = document.getElementById("regModal");
 var regBtn = document.getElementById("regBtn");
 var span = document.getElementsByClassName("close");
 var loginLink = document.getElementById("loginLink");
-var regLink = document.getElementById("regLink")
+var regLink = document.getElementById("regLink");
 
 loginBtn.onclick = () => {
   loginModal.style.display = "block";
@@ -34,4 +34,71 @@ loginLink.onclick = () => {
 regLink.onclick = () => {
   loginModal.style.display = "none";
   regModal.style.display = "block";
+}
+
+function setInputDate() {
+
+  var d = new Date();
+  var maxd = new Date();
+  var t = d.toTimeString().slice(0,3);
+
+  //set input time
+  document.getElementById("inputTime").value = document.getElementById("inputTime").min;
+  if ( parseInt(t) < 16 ) {
+    if( parseInt(t) >= 9) {
+      document.getElementById("inputTime").value = d.toTimeString().slice(0,3) + "00";
+      var maxHour = 16 - parseInt(t);
+      document.getElementById("inputHr").max = maxHour.toString();
+    }
+  }
+  else {
+    d.setDate(d.getDate()+1);
+    maxd.setDate(maxd.getDate()+1);
+  }
+
+  //set input date
+  maxd.setDate(maxd.getDate() + 6);
+  document.getElementById("inputDay").min = d.toISOString().slice(0,10);
+  document.getElementById("inputDay").max = maxd.toISOString().slice(0,10);
+  document.getElementById("inputDay").value = document.getElementById("inputDay").min;
+  
+  //set table date
+  document.getElementById("d1").innerHTML = d.toLocaleDateString();
+  d.setDate(d.getDate() + 1);
+  document.getElementById("d2").innerHTML = d.toLocaleDateString();
+  d.setDate(d.getDate() + 1);
+  document.getElementById("d3").innerHTML = d.toLocaleDateString();
+  d.setDate(d.getDate() + 1);
+  document.getElementById("d4").innerHTML = d.toLocaleDateString();
+  d.setDate(d.getDate() + 1);
+  document.getElementById("d5").innerHTML = d.toLocaleDateString();
+  d.setDate(d.getDate() + 1);
+  document.getElementById("d6").innerHTML = d.toLocaleDateString();
+  d.setDate(d.getDate() + 1);
+  document.getElementById("d7").innerHTML = d.toLocaleDateString();
+}
+
+function timeStepUp() {
+  document.getElementById("inputTime").stepUp(60);
+  //update inputHr max
+  var num = parseInt(document.getElementById("inputHr").max);
+  if (num > 1) {
+    num -= 1;
+    console.log("num- : "+num);
+    document.getElementById("inputHr").max = num.toString();
+    if(document.getElementById("inputHr").value > document.getElementById("inputHr").max) {
+      document.getElementById("inputHr").value = document.getElementById("inputHr").max;
+    }
+  }
+}
+
+function timeStepDown() {
+  document.getElementById("inputTime").stepDown(60);
+  //update inputHr max
+  var num = parseInt(document.getElementById("inputHr").max);
+  if (num < 7) {
+    num += 1;
+    console.log("num+ : "+num);
+    document.getElementById("inputHr").max = num.toString();
+  }
 }
