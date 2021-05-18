86c01d089cf163280110ca00d7216b85550fa92c
SaltAtWork <60347847+SaltAtWork@users.noreply.github.com>
2021-05-18 12:40:36 +0700

    Add files via upload

---
+++ Controllers/BlacklistController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Dinolab.Models;
+
+namespace Dinolab.Controllers
+{
+    public class BlacklistController : Controller
+    {
+        private readonly ILogger<BlacklistController> _logger;
+
+        public BlacklistController(ILogger<BlacklistController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
