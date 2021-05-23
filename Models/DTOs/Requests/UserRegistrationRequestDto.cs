eb4f50c2aa915d2f78a6329a6104893f8c0daab5
kumaru09 <ueki2542@gmail.com>
2021-05-23 17:57:54 +0700

    Update UserRegistrationRequestDto.cs

--- Models/DTOs/Requests/UserRegistrationRequestDto.cs
+++ Models/DTOs/Requests/UserRegistrationRequestDto.cs
@@ -4,11 +4,17 @@ namespace Dinolab.Models.DTOs.Requests
 {
     public class UserRegistrationRequestDTO
     {
+
         [Required]
         [EmailAddress]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
         [Required]
         public string Password { get; set; }
+        
+        [Required]
+        [RegularExpression(@"^\w{3,}$", ErrorMessage = "The field Username must have more than three and not have special character")]
         public string Username { get; set; }
     }
 }
